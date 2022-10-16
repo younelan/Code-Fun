566dd4cf9280bc05a33801e0ae5f78148414a74a
Youness El Andaloussi <youness@elandaloussi.net>
2022-10-16 00:25:07 -0400

    Algohelper extension is now json to help with syntax highlight

--- algo/AlgoHelper/testing.py
+++ algo/AlgoHelper/testing.py
@@ -1,7 +1,7 @@
 import json
 
 def load_tests(module_name):
-	file_name = "tests/%s.txt" % module_name
+	file_name = "tests/%s.json" % module_name
 	with open(file_name) as fp:
 		contents = fp.read()
 		retval = json.loads(contents)
