0b78720cecdb520c95f1bead5b45df751ca3115b
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-20 23:31:48 +0100

    Add Description+Empty Problem+Solution: bstInsertRemove

--- InterviewTraining/js/solutions/bstInsertRemove.js
+++ InterviewTraining/js/solutions/bstInsertRemove.js
@@ -68,28 +68,72 @@ class BST {
     }
 
     insert(value) {
-        // Write your code here
-        // Return this BST
+        if (value < this.value) {
+            if (this.left === null) {
+                this.left = new BST(value);
+            } else {
+                this.left.insert(value);
+            }
+        } else {
+            if (this.right === null) {
+                this.right = new BST(value);
+            } else {
+                this.right.insert(value);
+            }
+        }
         return this;
     }
 
     contains(value) {
-        // Write your code here
-        // Return true if value exists in the BST, false otherwise
-        return false;
+        if (value === this.value) {
+            return true;
+        } else if (value < this.value) {
+            return this.left ? this.left.contains(value) : false;
+        } else {
+            return this.right ? this.right.contains(value) : false;
+        }
     }
 
     remove(value, parent = null) {
-        // Write your code here
-        // Return this BST
+        if (value < this.value) {
+            if (this.left) {
+                this.left.remove(value, this);
+            }
+        } else if (value > this.value) {
+            if (this.right) {
+                this.right.remove(value, this);
+            }
+        } else {
+            if (this.left && this.right) {
+                this.value = this.right.getMinValue();
+                this.right.remove(this.value, this);
+            } else if (parent === null) {
+                if (this.left) {
+                    this.value = this.left.value;
+                    this.right = this.left.right;
+                    this.left = this.left.left;
+                } else if (this.right) {
+                    this.value = this.right.value;
+                    this.left = this.right.left;
+                    this.right = this.right.right;
+                } else {
+                    // Do nothing, this is a single-node tree; do not remove the root node.
+                }
+            } else if (parent.left === this) {
+                parent.left = this.left ? this.left : this.right;
+            } else if (parent.right === this) {
+                parent.right = this.left ? this.left : this.right;
+            }
+        }
         return this;
     }
 
     getMinValue() {
-        // Write your code here
-        // Helper method for remove
-        // Return the minimum value in this subtree
-        return 0;
+        if (this.left === null) {
+            return this.value;
+        } else {
+            return this.left.getMinValue();
+        }
     }
 }
 
