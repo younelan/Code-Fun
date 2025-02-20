176a5097fe77dbb182b89e0157d920cc80293fd8
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-21 00:48:46 +0100

    Add Description+Empty Problem+Solution: Caesar Cypher

--- InterviewTraining/js/solutions/caesarCipherEncryptor.js
+++ InterviewTraining/js/solutions/caesarCipherEncryptor.js
@@ -37,10 +37,16 @@ Important Notes:
 Return:
 - A string with each character shifted by 'key' positions
 */
-
 function caesarCipherEncryptor(string, key) {
-    // Write your code here
-    return "";
+    if (key > 25) {
+        key = key % 26;
+    }
+    let retval = "";
+    for (const ch of string) {
+        const newval = "a".charCodeAt(0) + (ch.charCodeAt(0) - "a".charCodeAt(0) + key) % 26;
+        retval += String.fromCharCode(newval);
+    }
+    return retval;
 }
 
 module.exports = caesarCipherEncryptor;
