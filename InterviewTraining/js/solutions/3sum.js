9905efe7558586207e5d7aab989d3d3d13ea7214
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-20 23:22:55 +0100

    Add Description+Empty Problem+Solution: 3Sum

--- InterviewTraining/js/solutions/3sum.js
+++ InterviewTraining/js/solutions/3sum.js
@@ -19,8 +19,17 @@ Space Complexity: O(n) - where n is the number of triplets found
 */
 
 function threeNumberSum(array, targetSum) {
+    const arrSorted = array.slice().sort((a, b) => a - b);
     const retval = [];
-
+    for (let i = 0; i < arrSorted.length - 2; i++) {
+        for (let j = i + 1; j < arrSorted.length - 1; j++) {
+            for (let k = j + 1; k < arrSorted.length; k++) {
+                if (arrSorted[i] + arrSorted[j] + arrSorted[k] === targetSum) {
+                    retval.push([arrSorted[i], arrSorted[j], arrSorted[k]]);
+                }
+            }
+        }
+    }
     return retval;
 }
 
