95a71584885dd79b6e430ffe8bf9b307a0d91fb0
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-21 00:54:35 +0100

    Add Description+Empty Problem+Solution: Class Photos

--- InterviewTraining/js/solutions/classPhotos.js
+++ InterviewTraining/js/solutions/classPhotos.js
@@ -44,8 +44,21 @@ Return:
 */
 
 function classPhotos(redShirtHeights, blueShirtHeights) {
-    // Write your code here
-    return false;
+    const sortedRed = redShirtHeights.slice().sort((a, b) => a - b);
+    const sortedBlue = blueShirtHeights.slice().sort((a, b) => a - b);
+    let blueFront = true;
+    let redFront = true;
+
+    for (let i = 0; i < sortedRed.length; i++) {
+        if (sortedRed[i] <= sortedBlue[i]) {
+            redFront = false;
+        }
+        if (sortedBlue[i] <= sortedRed[i]) {
+            blueFront = false;
+        }
+    }
+
+    return blueFront || redFront;
 }
 
 module.exports = classPhotos;
