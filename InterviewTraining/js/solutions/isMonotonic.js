1ad5d268b92bea1cdb40693bbee8d1cef1bb894e
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-22 09:11:25 +0100

    Add Description+Empty Problem+Solution: Is Monotonic

--- InterviewTraining/js/solutions/isMonotonic.js
+++ InterviewTraining/js/solutions/isMonotonic.js
@@ -41,7 +41,26 @@ Hints:
 */
 
 function isMonotonic(array) {
-    // Write your code here
+    if (array.length < 3) return true;
+
+    let prevElement = array[0];
+    let prevDirection = 0;
+
+    for (let i = 1; i < array.length; i++) {
+        const curElement = array[i];
+        let curDirection = 0;
+        if (curElement > prevElement) {
+            curDirection = 1;
+            if (prevDirection === -1) return false;
+        } else if (curElement < prevElement) {
+            curDirection = -1;
+            if (prevDirection === 1) return false;
+        }
+        prevElement = curElement;
+        if (prevDirection === 0) {
+            prevDirection = curDirection;
+        }
+    }
     return true;
 }
 
