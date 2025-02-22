87b43257164a6c09a69cbaaecff3cf0bd3b01971
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-22 09:03:52 +0100

    Add Description+Empty Problem+Solution: First Duplicate Value

--- InterviewTraining/js/solutions/firstDuplicateValue.js
+++ InterviewTraining/js/solutions/firstDuplicateValue.js
@@ -31,6 +31,13 @@ Hints:
 */
 
 function firstDuplicateValue(array) {
+    const counts = {};
+    for (const el of array) {
+        counts[el] = (counts[el] || 0) + 1;
+        if (counts[el] > 1) {
+            return el;
+        }
+    }
     return -1;
 }
 
