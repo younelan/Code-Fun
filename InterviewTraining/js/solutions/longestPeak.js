f4a99ee6555f03a0c073870a34adaa1f9a4956c1
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-23 09:28:18 +0000

    Add Description+Empty Problem+Solution: Longest Peak

--- InterviewTraining/js/solutions/longestPeak.js
+++ InterviewTraining/js/solutions/longestPeak.js
@@ -37,8 +37,27 @@ Notes:
 */
 
 function longestPeak(array) {
-    // Write your code here
-    return 0;
+    let maxPeak = 0;
+    if (array.length < 3) return 0;
+
+    let idx = 1;
+    while (idx < array.length - 1) {
+        const isPeak = array[idx - 1] < array[idx] && array[idx + 1] < array[idx];
+        if (isPeak) {
+            let start = idx;
+            while (start > 0 && array[start - 1] < array[start]) {
+                start--;
+            }
+            let end = idx;
+            while (end < array.length - 1 && array[end + 1] < array[end]) {
+                end++;
+            }
+            maxPeak = Math.max(maxPeak, end - start + 1);
+            idx = end;
+        }
+        idx++;
+    }
+    return maxPeak;
 }
 
 module.exports = longestPeak;
