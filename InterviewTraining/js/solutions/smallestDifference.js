5c03425da0b57e428e499ac3e2ec4594ad432967
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-25 01:05:26 +0000

    Add Description+Empty Problem+Solution: Smallest Difference

--- InterviewTraining/js/solutions/smallestDifference.js
+++ InterviewTraining/js/solutions/smallestDifference.js
@@ -43,8 +43,17 @@ Notes:
 */
 
 function smallestDifference(arrayOne, arrayTwo) {
-    // Write your code here
+    let minDiff = Infinity;
     let result = [];
+    for (let i = 0; i < arrayOne.length; i++) {
+        for (let j = 0; j < arrayTwo.length; j++) {
+            const diff = Math.abs(arrayOne[i] - arrayTwo[j]);
+            if (diff < minDiff) {
+                minDiff = diff;
+                result = [arrayOne[i], arrayTwo[j]];
+            }
+        }
+    }
     return result;
 }
 
