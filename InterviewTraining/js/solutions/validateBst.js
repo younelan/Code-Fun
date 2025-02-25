d86bcb9051e50da15f31f7ec43c2e9fe61d03fa3
Youness El Andaloussi <youness@elandaloussi.net>
2025-02-25 00:58:46 +0000

    Add Description+Empty Problem+Solution: Validate BST

--- InterviewTraining/js/solutions/validateBst.js
+++ InterviewTraining/js/solutions/validateBst.js
@@ -51,8 +51,13 @@ Hints:
 */
 
 function validateBst(tree, minVal = -Infinity, maxVal = Infinity) {
-    // Write your code here
-    return false;
+    if (!tree) return true;
+    if (tree.value < minVal || tree.value >= maxVal) return false;
+
+    const leftValid = validateBst(tree.left, minVal, tree.value);
+    const rightValid = validateBst(tree.right, tree.value, maxVal);
+
+    return leftValid && rightValid;
 }
 
 module.exports = validateBst;
